e1a4fe84807bf43509126688a1758167857e69a9
Andres Tombo <andrestombo2004@gmail.com>
2024-04-17 20:15:39 -0300

    creada lista + search add delete

--- WinForm/Form1.Designer.cs
+++ WinForm/Form1.Designer.cs
@@ -30,13 +30,18 @@
         {
             this.TxtBoxBuscarArticulo = new System.Windows.Forms.TextBox();
             this.LblArticulo = new System.Windows.Forms.Label();
-            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.btnBusca = new System.Windows.Forms.Button();
+            this.pnlLista = new System.Windows.Forms.Panel();
+            this.flpLista = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.pnlLista.SuspendLayout();
             this.SuspendLayout();
             // 
             // TxtBoxBuscarArticulo
             // 
             this.TxtBoxBuscarArticulo.AccessibleName = "TxtBoxBuscarArticulo";
-            this.TxtBoxBuscarArticulo.Location = new System.Drawing.Point(542, 24);
+            this.TxtBoxBuscarArticulo.Location = new System.Drawing.Point(60, 24);
             this.TxtBoxBuscarArticulo.Name = "TxtBoxBuscarArticulo";
             this.TxtBoxBuscarArticulo.Size = new System.Drawing.Size(173, 20);
             this.TxtBoxBuscarArticulo.TabIndex = 0;
@@ -45,34 +50,87 @@
             // 
             this.LblArticulo.AccessibleName = "LblArticulo";
             this.LblArticulo.AutoSize = true;
-            this.LblArticulo.Location = new System.Drawing.Point(494, 27);
+            this.LblArticulo.Location = new System.Drawing.Point(12, 27);
             this.LblArticulo.Name = "LblArticulo";
             this.LblArticulo.Size = new System.Drawing.Size(42, 13);
             this.LblArticulo.TabIndex = 1;
             this.LblArticulo.Text = "Articulo";
             // 
-            // BtnBuscar
+            // btnBusca
             // 
-            this.BtnBuscar.AccessibleName = "BtnBuscar";
-            this.BtnBuscar.Enabled = false;
-            this.BtnBuscar.Location = new System.Drawing.Point(721, 22);
-            this.BtnBuscar.Name = "BtnBuscar";
-            this.BtnBuscar.Size = new System.Drawing.Size(56, 23);
-            this.BtnBuscar.TabIndex = 2;
-            this.BtnBuscar.Text = "Buscar";
-            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.btnBusca.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnBusca.Location = new System.Drawing.Point(239, 24);
+            this.btnBusca.Name = "btnBusca";
+            this.btnBusca.Size = new System.Drawing.Size(56, 22);
+            this.btnBusca.TabIndex = 3;
+            this.btnBusca.Text = "Buscar";
+            this.btnBusca.UseVisualStyleBackColor = true;
+            // 
+            // pnlLista
+            // 
+            this.pnlLista.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlLista.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLista.Controls.Add(this.flpLista);
+            this.pnlLista.Location = new System.Drawing.Point(15, 51);
+            this.pnlLista.Name = "pnlLista";
+            this.pnlLista.Size = new System.Drawing.Size(773, 486);
+            this.pnlLista.TabIndex = 5;
+            // 
+            // flpLista
+            // 
+            this.flpLista.AutoScroll = true;
+            this.flpLista.AutoSize = true;
+            this.flpLista.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flpLista.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpLista.Location = new System.Drawing.Point(0, 0);
+            this.flpLista.Name = "flpLista";
+            this.flpLista.Size = new System.Drawing.Size(771, 484);
+            this.flpLista.TabIndex = 0;
+            this.flpLista.SizeChanged += new System.EventHandler(this.flpLista_SizeChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAgregar.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnAgregar.Location = new System.Drawing.Point(731, 24);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(56, 22);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEliminar.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnEliminar.Location = new System.Drawing.Point(669, 24);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(56, 22);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AccessibleName = "FormPrincipal";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 539);
-            this.Controls.Add(this.BtnBuscar);
+            this.ClientSize = new System.Drawing.Size(800, 549);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.pnlLista);
+            this.Controls.Add(this.btnBusca);
             this.Controls.Add(this.LblArticulo);
             this.Controls.Add(this.TxtBoxBuscarArticulo);
+            this.MinimumSize = new System.Drawing.Size(195, 250);
             this.Name = "Form1";
             this.Text = "Actividad 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlLista.ResumeLayout(false);
+            this.pnlLista.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,7 +140,11 @@
 
         private System.Windows.Forms.TextBox TxtBoxBuscarArticulo;
         private System.Windows.Forms.Label LblArticulo;
-        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button btnBusca;
+        private System.Windows.Forms.Panel pnlLista;
+        private System.Windows.Forms.FlowLayoutPanel flpLista;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
--- WinForm/Form1.cs
+++ WinForm/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,65 @@ namespace WinForm
 {
     public partial class Form1 : Form
     {
+        private int previewArtSize = 125;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AgregarArticulo();
+            AgregarArticulo();
+            AgregarArticulo();
+            AgregarArticulo();
+            UpdateFlpListaArticle();
+        }
+
+        private void AgregarArticulo()
+        {
+            PictureBox pBox = new PictureBox()
+            {
+                Anchor = AnchorStyles.None,
+                BackColor = Color.Thistle,
+                MinimumSize = new Size(previewArtSize, previewArtSize),
+                Size = new Size(previewArtSize, previewArtSize),
+            };
+            flpLista.SuspendLayout();
+            flpLista.Controls.Add(pBox);
+            flpLista.ResumeLayout(true);
+            UpdateFlpListaArticle();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            AgregarArticulo();
+        }
+
+        private void flpLista_SizeChanged(object sender, EventArgs e)
+        {
+            UpdateFlpListaArticle();
+
+        }
+
+        private void UpdateFlpListaArticle()
+        {
+            int calculatedWidth = (flpLista.Size.Width - 25);
+            float widthItemAmount = calculatedWidth / previewArtSize;
+
+            for (int i = 0; i < flpLista.Controls.Count; i++)
+            {
+                if (calculatedWidth < flpLista.Controls.Count * previewArtSize)
+                {
+                    flpLista.Controls[i].Size = new Size(calculatedWidth / (int)Math.Floor(widthItemAmount), 125);
+                    Debug.WriteLine(flpLista.Controls[i].Size);
+                }
+                else
+                {
+                    flpLista.Controls[i].Size = new Size(previewArtSize, previewArtSize);
+                }
+            }
         }
     }
 }
